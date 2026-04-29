c48e6f1e88a32c384964980bc12296eb0223a37e
tejveer008 <tejveers@gadieltechnologie.com>
2026-04-29 11:12:11 +0530

    CL MAIN

--- checklist/cl_main.py
+++ checklist/cl_main.py
@@ -1,12 +1,16 @@
+from pathlib import Path
+
 import streamlit as st
+
+CURRENT_DIR = Path(__file__).resolve().parent
 
 pages = {
     "Projects": [
-        st.Page("eden.py", title="EDEN"),
-        st.Page("checklistews.py", title="EWS Checklist"),
-        st.Page("Wave City.py", title="Wave City Checklist"),
-        st.Page("CheckEligo.py", title="Eligo Checklist"),
-        st.Page("veridia.py", title="Veridia"),
+        st.Page(str(CURRENT_DIR / "eden.py"), title="EDEN"),
+        st.Page(str(CURRENT_DIR / "checklistews.py"), title="EWS Checklist"),
+        st.Page(str(CURRENT_DIR / "Wave City.py"), title="Wave City Checklist"),
+        st.Page(str(CURRENT_DIR / "CheckEligo.py"), title="Eligo Checklist"),
+        st.Page(str(CURRENT_DIR / "veridia.py"), title="Veridia"),
     ]
 }
 
